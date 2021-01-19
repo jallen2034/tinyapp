a68353a467ecf3007363a1b3ca06d41bb10c7b55
Jacob Allen <jallen2033@gmail.com>
2021-01-19 01:48:37 +0000

    added POST route for /urls!

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// include body parser module so we can submit POST requests with forms
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // set ejs as the template engine that we will want to use
 app.set("view engine", "ejs");
 
@@ -10,32 +14,62 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// app route for the /hello path of our app, this returns the { greeting: 'Hello World!' } object to our template
+// function to generate a random string
+// https://stackoverflow.com/questions/16106701/how-to-generate-a-random-string-of-letters-and-numbers-in-javascript
+function generateRandomString() {
+  const textLen = 6;
+  var text = "";
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+  
+  for (var i = 0; i < textLen; i++) {
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+ 
+  return text;
+}
+
+// GET app route for the /hello path of our app, this returns the { greeting: 'Hello World!' } object to our template
+// first argument of res.render() is the view we pass to the template engine
+// second argument of res.render() is the variable containing the 'stuff' we want to insert into that previous view
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
-  // first argument is the view we pass to the template engine
-  // second argument is the variable containing the 'stuff' we want to insert into that previous view
   res.render("hello_world", templateVars);
 });
 
-// app route handler for "/urls"
+// GET app route handler for "/urls"
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
 
-// app route for short urls
+// POST app route for "/urls"
+// Respond with 'Ok' (we will replace this)
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  const randomString = generateRandomString();
+  console.log(randomString);
+  res.send("Ok");      
+});
+
+// GET app route for /urls/new app route, remember this needs to come before the /urls/:shortURL app route!
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// GET app route for short urls
 // remember "req.params.shortURL" is shorthand for whatever the user inputted client side in the url /urls/:*HERE*
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
   res.render("urls_show", templateVars);
 });
 
-// app route for urls.json
+// GET app route for urls.json
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
